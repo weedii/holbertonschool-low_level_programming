64fe5cd0711ca9ffa0c008fe97e06195bed7f39c
weedii <abidiw293@gmail.com>
2022-09-29 23:29:45 +0100

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main() {
-	char ch='"Programming is like building a multilingual puzzle';
+	char ch='"Programming is like building a multilingual puzzle\' ;
 	
 	puts(ch);
 	return 0;
