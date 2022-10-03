919b4bb021d91afd00a2f0d7c941700eebbbc2b4
weedii <abidiw293@gmail.com>
2022-10-03 23:01:48 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,9 +5,17 @@
  */
 int main(void)
 {
+int i;
 
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+if (i == 9)
+{continue; }
+putchar(',');
+putchar(' ');
+}
 
-
-putchar('a');
+putchar('\n');
 return (0);
 }
