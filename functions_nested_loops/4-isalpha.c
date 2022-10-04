d900d6bd344874e63b41940982492a313d50d322
weedii <abidiw293@gmail.com>
2022-10-04 19:36:14 +0100

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _islower -  checks for lowercase character
+ * _isalpha -  checks for alphabetic character
  * Return: 0 or 1
  * 'c': the argument
  */
 
 int _isalpha(int c)
 {
-    if (isalpha(c))
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
