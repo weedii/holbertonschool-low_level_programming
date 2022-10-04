81c34bedc501d84128ec4147c3fea1ac5374c458
weedii <abidiw293@gmail.com>
2022-10-04 12:21:42 +0100

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function print alphabet x10
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	}
+	putchar('\n');
+}
