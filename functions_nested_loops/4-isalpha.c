889a8c31035f8c35861b96fce07068abbb6b7be1
weedii <abidiw293@gmail.com>
2022-10-04 19:32:36 +0100

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower -  checks for lowercase character
+ * Return: 0 or 1
+ * 'c': the argument
+ */
+
+int _isalpha(int c)
+{
+    if (isalpha(c))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
