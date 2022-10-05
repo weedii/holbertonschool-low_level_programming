bd9295c4fe4b9f9985e56a5fa079d226d2ba3986
weedii <abidiw293@gmail.com>
2022-10-05 10:12:13 +0100

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int n);
