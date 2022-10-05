2443c1e5ee0f6e3a95f3da3abefb0601ecf63ee0
weedii <abidiw293@gmail.com>
2022-10-05 21:43:00 +0100

    add x + y

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  print the last digit of a number
+ * Return: return the last digit of a number
+ * @n: the argument
+ */
+
+int add(int x, int y)
+{
+    return (x + y);
+}
