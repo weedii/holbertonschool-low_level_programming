ffe9d71a6bd4710c7430596ca86f649ecf8316dd
weedii <abidiw293@gmail.com>
2022-10-06 10:34:02 +0100

    Learn your times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,36 +4,36 @@
  */
 void times_table(void)
 {
-    int i, j, r, x, y;
+	int i, j, r, x, y;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            r = i * j;
-            if (r > 9)
-            {
-                x = r / 10;
-                y = r % 10;
-                _putchar(' ');
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			r = i * j;
+			if (r > 9)
+			{
+				x = r / 10;
+				y = r % 10;
+				_putchar(' ');
 
-                _putchar('0' + x);
-                _putchar('0' + y);
-            }
-            else
-            {
-                if (j != 0)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                _putchar('0' + r);
-            }
-            if (j != 9)
-            {
-                _putchar(',');
-            }
-        }
-        _putchar('\n');
-    }
+				_putchar('0' + x);
+				_putchar('0' + y);
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar('0' + r);
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
 }
