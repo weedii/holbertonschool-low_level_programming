4cc759d6176e2622b331d0eef7bf7e97d1899f74
weedii <abidiw293@gmail.com>
2022-10-07 11:00:34 +0100

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - checks for a digit (0 through 9)
+ * Return: a*b
+ * @a: the argument
+ * @b: the argument
+ */
+
+void print_numbers(void)
+{
+    int i;
+
+    for (i = 0; i <= 9; i++)
+    {
+        _putchar('0' + i);
+    }
+}
