a8159122bcb35fb2917b36694f22a5d09cde0aa6
weedii <abidiw293@gmail.com>
2022-10-11 10:07:30 +0100

    Half the lies they tell about me aren't true

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,17 @@
 void puts2(char *str)
 {
 	int i, x;
+	char *saut;
+	saut = str;
 
 	x = strlen(str);
 
-	for (i = 0; i < x; i++)
+	for (i = 0; i < x - 1; i += 2)
 	{
-		while (*str != '\0')
+		while (*saut != '\0')
 		{
-			_putchar(*str);
-			str += 2;
+			_putchar(*saut);
+			saut += 2;
 		}
 	}
 	_putchar('\n');
