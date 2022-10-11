66c0510c29b065546a6734b781edf31dda623226
weedii <abidiw293@gmail.com>
2022-10-11 13:36:17 +0100

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copie the string
+ * @dest: pointer point to a string
+ * @src: pointer point to a string
+ * Return: a copie of the string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *ch1, *ch2;
+
+	ch1 = dest;
+	ch2 = src;
+
+	while (*ch2 != '\0')
+	{
+		*ch1 = *ch2;
+		printf("%c", *ch2);
+		ch1++;
+		ch2++;
+	}
+	return (ch1);
+}
