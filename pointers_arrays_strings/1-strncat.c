e2f9a5f155583f1972331ab00de957e1fca14276
weedii <abidiw293@gmail.com>
2022-10-12 11:38:57 +0100

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings
+ * *_strncat - concatenates two strings using at most n bytes from src
  *@dest: pointer point to a string
  *@src: pointer point to a string
+ *@n: bytes from src
  * Return: concatenates two strings
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; dest[i] != '\0'; i++)
-        ;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-    for (j = 0; src[j] != '\0' && j < n; j++)
-    {
-        dest[i + j] = src[j];
-    }
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
 
-    return (dest);
+	return (dest);
 }
