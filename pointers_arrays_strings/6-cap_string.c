f6e951c1f23b64004562498f4fa9d5d485b66af6
weedii <abidiw293@gmail.com>
2022-10-13 12:40:17 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalize all words of a string
+ * @s: pointer point to a string
+ * Return: capitalize all words of a string
+ */
+
+char *cap_string(char *s)
+{
+	char o[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 12; j++)
+		{
+			if (s[i] == o[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+
+	return (s);
+}
