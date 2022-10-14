a3bd89a428ea3a51048dbcf4f71b7bcb7fb83fe1
weedii <abidiw293@gmail.com>
2022-10-14 12:13:00 +0100

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings
+ * *_memcpy - copies memory area
  *@dest: pointer point to a string
  *@src: pointer point to a string
+ *@n:  bytes from memory area src to memory area dest
  * Return: concatenates two strings
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
-    return (dest);
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
