89d77666aa01fba03573e4367d5ec3e08a3c8cd7
weedii <abidiw293@gmail.com>
2022-10-14 12:06:59 +0100

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
     int i;
 
-    for (i = n; i > 0; i--)
+    for (i = 0; i < n; i++)
     {
         dest[i] = src[i];
     }
-    dest[i] = 0;
     return (dest);
 }
