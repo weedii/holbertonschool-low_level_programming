64e80b734381ab91b9e1bba175d1c326ee663968
weedii <abidiw293@gmail.com>
2022-10-14 12:41:22 +0100

    srtchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer point to an int
+ * @b: pointer point to an int
+ */
+
+char _strchr(char *s, char c)
+{
+    while (*s != '\0')
+    {
+        if (c == *s)
+        {
+            return (*s);
+        }
+
+        s++;
+    }
+    return (*s);
+}
