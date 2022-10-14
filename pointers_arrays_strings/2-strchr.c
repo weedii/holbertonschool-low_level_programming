db616314d9e8cdf854a676c87fb31dbb0ba07e5c
weedii <abidiw293@gmail.com>
2022-10-14 13:01:09 +0100

    srtchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers
- * @a: pointer point to an int
- * @b: pointer point to an int
+ * *_strchr - locate a character in a string
+ * @s: pointer point to a string
+ * @c: the first occurrence character
+ * Return: a pointer to the first occurrence of c in s
+ * or NULL if the character is not found
  */
 
-char _strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (c == *s)
-        {
-            return (*s);
-        }
-        else
-            *s = NULL;
-
-        s++;
-    }
-    return (*s);
+	while (*s != '\0')
+	{
+		if (c == *s)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
 }
