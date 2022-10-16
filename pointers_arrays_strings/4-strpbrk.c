ebf450dba9761fc33c0c55dcb39e2dc52b1fe49d
weedii <abidiw293@gmail.com>
2022-10-16 18:04:25 +0100

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - searche a string for any of a set of bytes
+ * @s: pointer point to array
+ * @accept: zize of the array
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	while (*s != '\0')
+	{
+		while (*s == *accept)
+		{
+			return (s - 1);
+		}
+		accept++;
+		s++;
+	}
+	return (s);
+}
