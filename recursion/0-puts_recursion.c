c73f85431d0bc45c2fe5880c4cea6666b8b5380f
weedii <abidiw293@gmail.com>
2022-10-17 10:51:02 +0100

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  print a string
+ * @s: pointer to a string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
