e38f548a4da9bbb6e2340cd2639d6060ede7e7dd
weedii <abidiw293@gmail.com>
2022-10-20 12:03:02 +0100

    Minimal Number of Coins for Change

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - program that multiplies two numbers
+ * main - program that prints the minimum number of coins-
+ * to make change for an amount of money
  * @argc: argument count
  * @argv: rgument vector
  * Return: 0
@@ -9,33 +10,33 @@
 
 int main(int argc, char const *argv[])
 {
-    int rest = 0, n;
+	int rest = 0, n;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    n = atoi(argv[1]);
+	n = atoi(argv[1]);
 
-    while (n > 0)
-    {
-        if (n >= 25)
-            n -= 25;
-        else if (n >= 10)
-            n -= 10;
-        else if (n >= 5)
-            n -= 5;
-        else if (n >= 2)
-            n -= 2;
-        else
-            n--;
+	while (n > 0)
+	{
+		if (n >= 25)
+			n -= 25;
+		else if (n >= 10)
+			n -= 10;
+		else if (n >= 5)
+			n -= 5;
+		else if (n >= 2)
+			n -= 2;
+		else
+			n--;
 
-        rest++;
-    }
+		rest++;
+	}
 
-    printf("%d\n", rest);
+	printf("%d\n", rest);
 
-    return (0);
+	return (0);
 }
