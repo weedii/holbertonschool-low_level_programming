a68f8174e0214e1844c25933d7e665686a7b9c1a
weedii <abidiw293@gmail.com>
2022-10-21 15:21:03 +0100

    He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,29 +9,29 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    char *ch;
-    int i, j, len, len1, len_concat;
+	char *ch;
+	int i, j, len, len1, len_concat;
 
-    if (s1 == NULL || s2 == NULL)
-        return (NULL);
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 
-    len = strlen(s1);
-    len1 = strlen(s2);
-    len_concat = len + len1;
+	len = strlen(s1) + 1;
+	len1 = strlen(s2) + 1;
+	len_concat = len + len1;
 
-    ch = malloc(sizeof(char) * len_concat);
+	ch = malloc(sizeof(char) * len_concat);
 
-    if (ch == NULL)
-        return (NULL);
+	if (ch == NULL)
+		return (NULL);
 
-    for (i = 0; i < len_concat; i++)
-        ch[i] = s1[i];
-    for (i = 0; ch[i] != '\0'; i++)
-        ;
-    for (j = 0; s2[j] != '\0'; j++)
-    {
-        ch[i + j] = s2[j];
-    }
+	for (i = 0; i < len_concat; i++)
+		ch[i] = s1[i];
+	for (i = 0; ch[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		ch[i + j] = s2[j];
+	}
 
-    return (ch);
+	return (ch);
 }
