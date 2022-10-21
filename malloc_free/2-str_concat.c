26c2d513186137bb91c04f0e020c7cefadc7c402
weedii <abidiw293@gmail.com>
2022-10-21 17:40:55 +0100

    He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strdup - duplicate a string
- * @str: pointer to a string
- * Return: pointer to a newly allocated space in memory which contains
- *		a copy of the string given as a parameter or NULL if str = NULL
+ * str_concat - concatenate two strings
+ * @s1: pointer to a string
+ * @s2: pointer to a string
+ * Return: return NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
@@ -15,8 +15,8 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
-	len = strlen(s1) + 1;
-	len1 = strlen(s2) + 1;
+	len = strlen(s1);
+	len1 = strlen(s2);
 	len_concat = len + len1;
 
 	ch = malloc(sizeof(char) * len_concat);
@@ -26,12 +26,12 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len_concat; i++)
 		ch[i] = s1[i];
+
 	for (i = 0; ch[i] != '\0'; i++)
 		;
+
 	for (j = 0; s2[j] != '\0'; j++)
-	{
 		ch[i + j] = s2[j];
-	}
 
 	return (ch);
 }
