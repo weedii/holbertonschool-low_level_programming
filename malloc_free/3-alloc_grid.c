05a51fe3235cd4f7f4343f46b180a5608012ece9
weedii <abidiw293@gmail.com>
2022-10-23 00:23:40 +0100

    If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,7 +9,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **g, i;
+	int **g, i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -17,15 +17,27 @@ int **alloc_grid(int width, int height)
 	g = malloc(sizeof(int *) * height);
 
 	if (g == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < height; i++)
+		g[i] = malloc(sizeof(int) * width);
+
+		if (g[i] == 0)
+		{
+			i--;
 			free(g[i]);
+		}
+		free(g);
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
-		g[i] = malloc(sizeof(int) * width);
-
+	{
+		for (j = 0; j < width; j++)
+		{
+			g[i][j] = 0;
+		}
+	}
 	return (g);
-	free(g);
 }
