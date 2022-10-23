6c458e1197a870e0e951e0d5b92f6a8c4e1f2e19
weedii <abidiw293@gmail.com>
2022-10-23 09:50:15 +0100

    It's not bragging if you can back it up

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: number of columns
+ * @height: number of rows
+ * Return: returns a pointer to a 2 dimensional array of integers
+ */
+
+void free_grid(int **grid, int height)
+{
+    int i;
+
+    for (i = 0; i < height; i++)
+        free(grid[i]);
+}
