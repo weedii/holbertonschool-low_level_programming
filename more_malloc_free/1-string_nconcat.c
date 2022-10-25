99e0ac662dca2d09b514b6b3f9898d4f060a932a
weedii <abidiw293@gmail.com>
2022-10-25 20:05:33 +0100

    string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,12 +14,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len = (strlen(s1) + strlen(s2) - 1), len_s2 = strlen(s2), i, j;
 	char *ch;
 
-	if (s1 == NULL)
+	if (s1 == NULL || *s1 == "")
 	{
 		s1 = "";
 		return (NULL);
 	}
-	if (s2 == NULL)
+	if (s2 == NULL || *s2 == "")
 	{
 		s2 = "";
 		return (NULL);
