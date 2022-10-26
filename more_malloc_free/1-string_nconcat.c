9a76297f14c0b99fd50383fa4c127137cbd39697
weedii <abidiw293@gmail.com>
2022-10-26 09:13:15 +0100

    string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len_s2)
 		ch = malloc(sizeof(char) * len_concat);
 	else
-		ch = malloc(sizeof(char) * len_s1 + n);
+		ch = malloc(sizeof(char) * len_s1 + n + 1);
 
 	if (ch == NULL)
 		return (NULL);
