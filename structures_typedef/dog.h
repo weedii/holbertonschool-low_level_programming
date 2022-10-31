08bd5015608cabd11765cc605c122739a2ae0b18
weedii <abidiw293@gmail.com>
2022-10-31 12:41:05 +0100

    A door is what a dog is perpetually on the wrong side of

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - structure for a dog
  * @name: name of the dog
@@ -17,5 +20,6 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
