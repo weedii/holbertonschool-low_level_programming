77cf995a46be79b33bebb929124b69703b2e414d
weedii <abidiw293@gmail.com>
2022-10-31 17:52:03 +0100

    A door is what a dog is perpetually on the wrong side of

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,33 @@
 #include "dog.h"
 
 /**
+ * str_malloc - allocate string in memory
+ * @str:   pointer to a string
+ * Return: pointer to the allocated string
+ */
+
+char *str_malloc(char *str)
+{
+	char *ch;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	ch = malloc(sizeof(char) * (strlen(str)));
+
+	if (ch == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		ch[i] = str[i];
+
+	ch[i] = '\0';
+
+	return (ch);
+}
+
+/**
  * new_dog - creates a new dog
  * @name:  pointer to the name in the structure
  * @age:   pointer to the age in the structure
@@ -11,33 +38,25 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *name_copy, *owner_copy;
-	int i, j;
 
 	dog = malloc(sizeof(dog_t));
-	name_copy = malloc(sizeof(strlen(name)));
-	owner_copy = malloc(sizeof(strlen(owner)));
 
 	if (dog == NULL)
 		return (NULL);
 
-	if (name_copy == NULL)
+	dog->name = str_malloc(name);
+	if (dog->name == NULL)
 	{
-		free(name_copy);
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; name[i] != '\0'; i++)
-		name_copy[i] = name[i];
 
-	if (owner_copy == NULL)
+	dog->owner = str_malloc(owner);
+	if (dog->owner == NULL)
 	{
-		free(owner_copy);
 		free(dog);
 		return (NULL);
 	}
-	for (j = 0; owner_copy[j] != '\0'; j++)
-		owner_copy[j] = owner[j];
 
 	dog->age = age;
 
