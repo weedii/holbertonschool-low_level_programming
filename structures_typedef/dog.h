9fd632cb3771f2adf6d7e2706a4a1b08cdfdc5f6
weedii <abidiw293@gmail.com>
2022-10-31 10:04:46 +0100

    Poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
 struct dog
 {
-    char *name;
-    char *owner;
-    float age;
+	char *name;
+	char *owner;
+	float age;
 };
 
 #endif
