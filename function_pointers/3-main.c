724f3d576938d39a430a8c3e5fa6a48b2d5b822d
weedii <abidiw293@gmail.com>
2022-11-03 17:48:15 +0100

    main file

---
+++ function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main - the main fuction
+ * @argc: number of elements
+ * @argv: array of elements
+ * Return: (0)
+ */
+
+int main(int argc, char const *argv[])
+{
+	int (*calc)(int, int);
+	int res;
+	char opp;
+
+	opp = *(argv[2]);
+	calc = get_op_func(&opp);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (atoi(argv[3]) < 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = calc(atoi(argv[1]), atoi(argv[3]));
+	if (res == 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", res);
+
+	return (0);
+}
