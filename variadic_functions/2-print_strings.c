90052d92dda78b1f279b094fa99b81a3c79137e7
weedii <abidiw293@gmail.com>
2022-11-04 14:40:37 +0100

    One woman's constant is another woman's variable

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (i != n - 1)
 				printf("%s", separator);
 		}
-		printf("nil");
+		else
+			printf("nil");
 	}
 
 	putchar('\n');
