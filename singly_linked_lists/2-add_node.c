00c4a2bd2e797e09f85b5de2157e567c22156319
weedii <abidiw293@gmail.com>
2022-11-15 19:39:15 +0100

    Add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,6 +11,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr = malloc(sizeof(list_t));
 
+	if (ptr == NULL)
+		return (NULL);
 	if (head != NULL || ptr != NULL)
 	{
 		ptr->str = strdup(str);
@@ -18,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 		ptr->next = *head;
 		*head = ptr;
 		return (ptr);
-		free(ptr);
 	}
+	free(ptr);
 	return (NULL);
 }
