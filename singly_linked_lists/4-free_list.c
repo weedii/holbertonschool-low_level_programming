a202973687ecc5b6a1b8e4fac38fa0c35db9f0ca
weedii <abidiw293@gmail.com>
2022-11-15 22:35:38 +0100

    Free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer to the first node
+ */
+
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
