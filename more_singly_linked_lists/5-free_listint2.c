e1491267e4f03a3db0bab14487b77996b026b96f
weedii <abidiw293@gmail.com>
2022-11-16 21:24:42 +0100

    Free

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,22 @@
 #include "lists.h"
 
 /**
- * free_listint - function that frees a listint_t list
+ * free_listint2 - function that frees a listint_t list
  * @head: pointer to the first node
  */
 
 void free_listint2(listint_t **head)
 {
-    listint_t *first, *move = *head;
+	listint_t *first, *move;
 
-    while (move)
-    {
-        first = move;
-        move = move->next;
-        free(first);
-    }
-    *head = NULL;
+	if (head == NULL || *head == NULL)
+		return;
+	while (*head)
+	{
+		move = *head;
+		first = move;
+		move = move->next;
+		free(first);
+	}
+	move = NULL;
 }
