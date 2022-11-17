c51d69dee5c2d109a0b8a0b02c768879b2c1b1f0
weedii <abidiw293@gmail.com>
2022-11-17 11:45:34 +0100

    Get node at index

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -11,9 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int find = 0;
 
-	if (!head)
-		return (NULL);
-	while (head->next != NULL)
+	while (head)
 	{
 		if (find == index)
 			break;
