14287663a83e6a0c0bb185e79631470580685b6c
weedii <abidiw293@gmail.com>
2022-11-17 21:25:24 +0100

    Insert

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *first, *lengh = *head, *new_node = malloc(sizeof(listint_t));
 	unsigned int i = 0, count = 0;
 
+	/*test if index outside the list*/
 	while (lengh)
 	{
 		lengh = lengh->next;
@@ -23,27 +24,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->next = NULL;
 
+	/*test if Insert a node at the beginning of a list*/
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	else
-	{
-		first = *head;
-		while (i < idx - 1)
-		{
-			first = first->next;
-			i++;
-		}
 
-		new_node->next = first->next;
-		first->next = new_node;
+	first = *head;
+	while (i < idx - 1)
+	{
+		first = first->next;
+		i++;
 	}
+	new_node->next = first->next;
+	first->next = new_node;
+
 	return (new_node);
 }
