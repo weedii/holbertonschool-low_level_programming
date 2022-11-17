b03b17bd23149d6e2c2b3ae8113c531467621936
weedii <abidiw293@gmail.com>
2022-11-17 10:22:49 +0100

    Pop

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -7,13 +7,13 @@ int pop_listint(listint_t **head)
     listint_t *delte;
     int n;
 
-    if (head == NULL)
-        return (0);
-
-    delte = *head;
-    n = delte->n;
-    *head = (*head)->next;
-    free(delte);
-
-    return (n);
+    if (*head)
+    {
+        delte = *head;
+        n = delte->n;
+        *head = (*head)->next;
+        free(delte);
+        return (n);
+    }
+    return (0);
 }
