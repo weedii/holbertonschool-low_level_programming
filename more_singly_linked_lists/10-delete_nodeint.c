439da135601786d44a28bed6fdd53cc7841eafe3
weedii <abidiw293@gmail.com>
2022-11-18 12:54:34 +0100

    Delete at index

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -23,6 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head)
 	{
+		/*test if Insert a node at the beginning of a list*/
 		if (index == 0)
 		{
 			*head = first->next;
