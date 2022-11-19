7bde0df08adb892df8137cec0a569a262332cbd0
weedii <abidiw293@gmail.com>
2022-11-19 12:46:55 +0100

    header files

---
+++ bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILES
+#define HEADER_FILES
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
