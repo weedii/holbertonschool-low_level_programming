17a8336a0e3629c21dd9146a85f7163e107bdd95
weedii <abidiw293@gmail.com>
2022-11-21 20:47:21 +0100

    Tread lightly, she is near

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/***/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    FILE *fp;
+    char c;
+    size_t i = 0, j = 0;
+
+    if (!filename)
+        return (0);
+
+    fp = fopen(filename, "r");
+
+    while ((c = fgetc(fp)) && i < letters)
+    {
+        if (c != EOF)
+        {
+            putchar(c);
+            j++;
+        }
+        i++;
+    }
+    fclose(fp);
+
+    return (j);
+}
