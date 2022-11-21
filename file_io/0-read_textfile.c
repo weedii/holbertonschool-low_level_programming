e5281eecf621fd24de45119232f0c506fadd2be4
weedii <abidiw293@gmail.com>
2022-11-21 20:56:14 +0100

    Tread lightly, she is near

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,28 +1,37 @@
 #include "main.h"
 
-/***/
+/**
+ * read_textfile - function that reads a text file
+ *              and prints it to the POSIX standard output.
+ * @filename: contain the name of the file
+ * @letters:  number of chars to be read and printed
+ * Return:   the actual number of letters it could read and print
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *fp;
-    char c;
-    size_t i = 0, j = 0;
+	FILE *fp;
+	char c;
+	size_t i = 0, j = 0;
 
-    if (filename)
-    {
-        fp = fopen(filename, "r");
+	if (filename)
+	{
+		fp = fopen(filename, "r");
 
-        while ((c = fgetc(fp)) && i < letters)
-        {
-            if (c != EOF)
-            {
-                putchar(c);
-                j++;
-            }
-            i++;
-        }
-        fclose(fp);
-        return (j);
-    }
-    return (0);
+		if (fp == NULL)
+			return (0);
+
+		while ((c = fgetc(fp)) && i < letters)
+		{
+			if (c != EOF)
+			{
+				putchar(c);
+				j++;
+			}
+			i++;
+		}
+		fclose(fp);
+		return (j);
+	}
+	return (0);
 }
