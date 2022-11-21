698050c4b97bf652861548f5f59e93639ba7e23d
weedii <abidiw293@gmail.com>
2022-11-21 11:09:54 +0100

    1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,20 +1,13 @@
 #include "main.h"
 
-/***/
+/**
+ * print_binary - function that prints binary representation of a number
+ * @n: variable to be converted
+ */
 
 void print_binary(unsigned long int n)
 {
-    int c, result;
-
-    for (c = 10; c >= 0; c--)
-    {
-        result = n >> c;
-
-        if (result & 1)
-            _putchar('0' + 1);
-        else
-            _putchar('0' + 0);
-    }
-
-    printf("\n");
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
