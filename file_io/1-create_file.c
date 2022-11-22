8745cbd81692731f413d6493796951551d78a066
weedii <abidiw293@gmail.com>
2022-11-22 21:00:56 +0100

    Under the snow

---
+++ file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/***/
+
+int create_file(const char *filename, char *text_content)
+{
+    int fp, len = 0;
+
+    if (filename)
+    {
+        fp = open(filename, O_CREAT | O_RDWR, 0600);
+        if (fp == -1)
+            return (0);
+
+        if (!text_content)
+        {
+            close(fp);
+            return (1);
+        }
+        for (len = 0; text_content[len]; len++)
+            ;
+
+        write(fp, text_content, len);
+        close(fp);
+        return (1);
+    }
+    return (-1);
+}
