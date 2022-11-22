63fd4afe5dc9c86cb9e6e865cda8c306a6eeee69
weedii <abidiw293@gmail.com>
2022-11-22 21:17:39 +0100

    Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,28 +1,33 @@
 #include "main.h"
 
-/***/
+/**
+ * create_file - function that creates a file
+ * @filename:     the name of the file to create
+ * @text_content: a text to write in the file
+ * Return:        Returns: 1 on success, -1
+ */
 
 int create_file(const char *filename, char *text_content)
 {
-    int fp, len = 0;
+	int fp, len = 0;
 
-    if (filename)
-    {
-        fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        if (fp == -1)
-            return (-1);
+	if (filename)
+	{
+		fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		if (fp == -1)
+			return (-1);
 
-        if (!text_content)
-        {
-            close(fp);
-            return (1);
-        }
-        for (len = 0; text_content[len]; len++)
-            ;
+		if (!text_content)
+		{
+			close(fp);
+			return (1);
+		}
+		for (len = 0; text_content[len]; len++)
+			;
 
-        write(fp, text_content, len);
-        close(fp);
-        return (1);
-    }
-    return (-1);
+		write(fp, text_content, len);
+		close(fp);
+		return (1);
+	}
+	return (-1);
 }
