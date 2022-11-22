a9589d16e87d9b4a0c225567687024a76fe54068
weedii <abidiw293@gmail.com>
2022-11-22 13:07:26 +0100

    Tread lightly, she is near

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -24,6 +24,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			return (0);
 
 		i = read(fp, c, letters);
+		if (i == -1)
+			return (0);
+
 		write(STDOUT_FILENO, c, letters);
 		free(c);
 		close(fp);
