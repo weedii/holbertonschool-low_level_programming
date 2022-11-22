0a99a69a6ebb32739e3e4a5dae7f6dcee116b3d3
weedii <abidiw293@gmail.com>
2022-11-22 21:15:00 +0100

    Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@ int create_file(const char *filename, char *text_content)
 
     if (filename)
     {
-        fp = open(filename, O_CREAT | O_RDWR, 0600);
+        fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
         if (fp == -1)
             return (-1);
 
