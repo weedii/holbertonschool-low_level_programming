bb9313025249a31b400aa0fc5bcfaf3a194e29e5
weedii <abidiw293@gmail.com>
2022-11-24 13:20:55 +0100

    Free list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,15 +1,18 @@
 #include "lists.h"
 
-/***/
+/**
+ * free_dlistint - unction that frees a dlistint_t list
+ * @head: pointer to the first node
+ */
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *prey = head;
+	dlistint_t *prey = head;
 
-    while (head)
-    {
-        free(prey);
-        head = head->next;
-        prey = head;
-    }
+	while (head)
+	{
+		free(prey);
+		head = head->next;
+		prey = head;
+	}
 }
