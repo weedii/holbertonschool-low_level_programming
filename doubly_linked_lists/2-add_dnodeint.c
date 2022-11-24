717fd35472a2027244a69d150fbd5959bfefdc0d
weedii <abidiw293@gmail.com>
2022-11-24 11:40:10 +0100

    Add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -18,8 +18,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new_node->n = n;
+	new_node->prev = NULL;
 	new_node->next = *head;
-	(*(head))->prev = new_node;
+
+	if (*head)
+		(*head)->prev = new_node;
 	*head = new_node;
-	return (new_node);
+
+	return (*head);
 }
