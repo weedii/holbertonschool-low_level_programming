057fb6b66b868fe7a2ee08ff9c9a917e3deac4c3
weedii <abidiw293@gmail.com>
2022-11-25 12:41:54 +0100

    Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	/*test if Insert a node at the end of a list*/
-	if (ptr->next == NULL)
+	if ((*h)->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	ptr = *h;
