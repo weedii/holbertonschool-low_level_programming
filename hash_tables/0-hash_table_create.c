31ae14a9b88d7025123d84942f25ae4690e400bd
weedii <abidiw293@gmail.com>
2022-11-26 23:18:44 +0100

    creates a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,6 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *_hash_table = malloc(sizeof(hash_table_t));
 
+	if (!_hash_table)
+		return (NULL);
+
 	_hash_table->size = size;
 	_hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
