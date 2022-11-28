0a8860bb795c3661c7652f7061bd41467442d978
weedii <abidiw293@gmail.com>
2022-11-28 11:12:26 +0100

    add an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -7,14 +7,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     hash_node_t *new_element = malloc(sizeof(hash_node_t));
     unsigned long int index;
 
-    if (!new_element)
-        return (0);
+    if (ht)
+    {
+        if (!new_element)
+            return (0);
 
-    new_element->key = strdup(key);
-    new_element->value = strdup(value);
+        new_element->key = strdup(key);
+        new_element->value = strdup(value);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    ht->array[index] = new_element;
+        index = key_index((const unsigned char *)key, ht->size);
+        ht->array[index] = new_element;
 
-    return (1);
+        return (1);
+    }
+    return (0);
 }
