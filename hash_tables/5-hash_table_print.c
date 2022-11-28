469bcf4bcff5db0ba10bdc9c1f01817afe6f5787
weedii <abidiw293@gmail.com>
2022-11-28 17:51:53 +0100

    tetst

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -4,20 +4,30 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int j;
-    hash_node_t *p;
+    unsigned int i = 0, j = 0;
 
-    if (!ht)
+    if (ht)
     {
-        return;
-    }
-    for (j = 0; ht && j < ht->size; j++)
-    {
-        p = ht->array[j];
-        while (p)
+        putchar('{');
+        while (j < ht->size)
         {
-            printf("%s : %s ", p->key, p->value);
-            p = p->next;
+            while (ht->array[i])
+            {
+
+                if (!(ht->array[i]->next))
+                {
+                    printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+                }
+                else
+                {
+                    printf("'%s' : '%s', ", ht->array[i]->key, ht->array[i]->value);
+                }
+                ht->array[i] = ht->array[i]->next;
+            }
+            j++;
+            i++;
         }
+        putchar('}');
+        putchar('\n');
     }
 }
