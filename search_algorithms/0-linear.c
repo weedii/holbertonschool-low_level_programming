e17099d2a2be9938d2a467f60be4077378495b7a
weedii <abidiw293@gmail.com>
2023-01-03 20:30:24 +0100

    Linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,6 +15,7 @@ int linear_search(int *array, size_t size, int value)
         }
         if (value == array[i])
         {
+            printf("Value checked array[%ld] = [%d]\n", i, array[i]);
             return (i);
         }
     }
