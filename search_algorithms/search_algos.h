eb23d8f814de8a32f7c73bff2513122c41c3d3a4
weedii <abidiw293@gmail.com>
2023-01-03 20:25:43 +0100

    header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef Monty
+#define Monty
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
