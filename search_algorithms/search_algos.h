05f7ef9e30dec6a8123bd1d85886fc2111617b08
weedii <abidiw293@gmail.com>
2023-01-07 22:41:48 +0100

    Jump search

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,13 +1,15 @@
-#ifndef Monty
-#define Monty
+#ifndef Search_algos
+#define Search_algos
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <stdbool.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binarysearch(int *array, int start, int size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
